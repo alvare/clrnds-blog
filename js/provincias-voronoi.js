d29efa8e3c99a9675af9b59553255884c17007fb
clrnd <welcometothechango@gmail.com>
2015-05-08 18:15:09 -0300

    dasdasd

--- js/provincias-voronoi.js
+++ js/provincias-voronoi.js
@@ -1,25 +1,34 @@
-"use strict";
+'use strict';
 
-var width = 780;
+var dest = d3.select('#container');
+var width = dest.node().getBoundingClientRect().width;
 var height = 500;
 
-var svg = d3.select("#container").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select('#container').append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
-d3.json("/files/provincias-topo.json", function(error, arg){
+var color = d3.scale.category20();
+
+d3.json('/files/provincias-topo.json', function(error, arg){
     if (error) return console.error(error);
 
     var proj = d3.geo.albers()
-        .center([-33, -64])
-        .rotate([0, 0, 0])
+        .center([-64, -33])
+        .rotate([55, 0, 0])
         .parallels([-20, -56])
-        .scale(361)
-        //.translate([width / 2, height / 2]);
+        .scale(750)
+        .translate([width / 2 - 520, 400]);
 
-    var datum = topojson.feature(arg, arg.objects.provincias);
+    var datum = topojson
+        .feature(arg, arg.objects.provincias)
+        .features;
 
-    svg.append("path")
-      .datum(datum)
-      .attr("d", d3.geo.path().projection(proj));
+    var path = d3.geo.path().projection(proj);
+
+    svg.selectAll('.provincia')
+      .data(datum)
+    .enter().append('path')
+      .attr('fill', function(e){ console.log(e.id);return color(e.id); })
+      .attr('d', path);
 });
--- posts/2015-05-07-primera-division-voronoi.md
+++ posts/2015-05-07-primera-division-voronoi.md
@@ -4,8 +4,9 @@ author: Ezequiel A. Alvarez
 tags: introduction
 ---
 
-Hola, World!
 <div id="container"></div>
+
+This map ...
 
 <script src="/js/topojson.min.js"></script>
 <script src="/js/provincias-voronoi.js"></script>
