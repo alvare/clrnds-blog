e9f4bbaaf6d3af22cef09f6e5392e6f197b35109
clrnd <welcometothechango@gmail.com>
2015-05-12 15:25:51 -0300

    terminando post voronoi

--- js/provincias-voronoi.js
+++ js/provincias-voronoi.js
@@ -1,79 +1,131 @@
 'use strict';
 
-var dest = d3.select('#container');
-var width = dest.node().getBoundingClientRect().width;
-var height = 640;
-
-var svg = d3.select('#container').append('svg')
-    .attr('width', width)
-    .attr('height', height);
-
 async.map(['/files/argentina-topo.json',
            '/files/equipos-geo.json'],
-    d3.json,
-    function(error, results){
-        if (error) return console.error(error);
+           d3.json,
+function(error, results){
+    if (error) return console.error(error);
 
-        var arg = results[0];
-        var equipos = results[1];
+    var arg = results[0];
+    var equipos = results[1];
 
-        var proj = d3.geo.albers()
-            .center([0, -39])
-            .rotate([65, 0, 0])
-            .parallels([-20, -56])
-            .scale(1000)
-            .translate([width / 2, height / 2]);
+    var width = d3.select('#article').node().getBoundingClientRect().width;
 
-        var border = topojson
-            .feature(arg, arg.objects.argentina);
 
-        var path = d3.geo.path()
-            .pointRadius(2)
-            .projection(proj);
+    var height_full = 640;
+    var proj_full = d3.geo.albers()
+        .center([0, -36])
+        .rotate([65, 0, 0])
+        .parallels([-20, -56])
+        .scale(1700)
+        .translate([width / 2, height_full / 2]);
 
-        var voronoi = d3.geom.voronoi()
-            .clipExtent([[0, 0], [width, height]]);
+    render(arg, equipos, '#container-full', width, height_full, proj_full);
 
-        // border
-        svg.append('path')
-            .datum(border)
-            .style({
-                'stroke': 'black',
-                'fill': 'none'
-            })
-            .attr('d', path);
+    var height_small = 500;
+    var proj_small = d3.geo.albers()
+        .center([0, -34.8])
+        .rotate([58.4, 0, 0])
+        .parallels([-20, -56])
+        .scale(29000)
+        .translate([width / 2, height_small / 2]);
 
-        // stadiums
-        svg.append('path')
-            .datum(equipos)
-            .attr('d', path);
+    render(arg, equipos, '#container-small', width, height_small, proj_small);
+});
 
-        // voronois
-        var points = equipos.features.map(function(d){
-            return proj(d.geometry.coordinates);
-        });
+function render(arg, equipos, selector, width, height, proj){
+    var dest = d3.select(selector);
+    var svg = dest.append('svg')
+        .attr('width', width)
+        .attr('height', height);
 
-        var exterior = projectLineString(border, proj);
+    var border = topojson
+        .feature(arg, arg.objects.argentina);
 
-        svg.append('g').selectAll('.voronoi')
-            .data(voronoi(points).map(function(d){
-                    return d3.geom.polygon(d).clip(exterior.slice());
-                })
-            )
-          .enter().append('path')
-            .style({
-                'stroke': 'black',
-                'stroke-opacity': '0.5',
-                'stroke-width': '.5px',
-                'fill': 'none'
-            })
-            .attr("d", function(d){
-                return "M" + d.join("L") + "Z";
-            });
+    var path = d3.geo.path()
+        .pointRadius(2)
+        .projection(proj);
+
+    var voronoi = d3.geom.voronoi();
+
+    // border
+    svg.append('path')
+        .datum(border)
+        .style({
+            'stroke': 'black',
+            'fill': '#eee'
+        })
+        .attr('class', 'argentina')
+        .attr('d', path);
+
+    // voronois
+    var points = equipos.features.map(function(d){
+        return proj(d.geometry.coordinates);
     });
 
-function projectLineString(feature, projection) {
-    function noop(){};
+    var exterior = projectLineString(border, proj);
+
+    var voronois = voronoi(points).map(function(d){
+        return d3.geom.polygon(d).clip(exterior.slice());
+    });
+
+    svg.append('g').selectAll('.voronoi')
+        .data(d3.zip(voronois, equipos.features))
+      .enter().append('g')
+        .on('mouseenter', highlight)
+        .on('mouseleave', unhighlight)
+      .append('path')
+        .style({
+            'stroke': 'black',
+            'stroke-opacity': '0.5',
+            'stroke-width': '.5px'
+        })
+        .attr("d", function(d){
+            return "M" + d[0].join("L") + "Z";
+        });
+
+    // stadiums
+    svg.append('path')
+        .datum(equipos)
+        .style({
+            'fill': '#333'
+        })
+        .attr('d', path);
+
+    function highlight(d){
+        var p = proj(d[1].geometry.coordinates);
+        var isleft = p[0] < width / 2;
+        var t = d3.select(this)
+            .classed('active', true);
+
+        t.append('text')
+            .attr('class', 'label')
+            .attr('x', p[0])
+            .attr('y', p[1])
+            .attr('dy', '-0em')
+            .attr('dx', isleft ? '10px' : '-10px')
+            .attr('text-anchor', isleft ? 'start' : 'end')
+            .text(d[1].properties.estadio);
+
+        t.append('text')
+            .attr('class', 'label label-small')
+            .attr('x', p[0])
+            .attr('y', p[1])
+            .attr('dy', '1.5em')
+            .attr('dx', isleft ? '10px' : '-10px')
+            .attr('text-anchor', isleft ? 'start' : 'end')
+            .text(d[1].properties.club);
+    }
+
+    function unhighlight(d){
+        d3.select(this)
+            .classed('active', false);
+        svg.selectAll('.label')
+            .remove();
+    }
+}
+
+function projectLineString(feature, projection){
 
     var line;
 
@@ -89,3 +141,4 @@ function projectLineString(feature, projection) {
     return line;
 }
 
+function noop(){};
--- posts/2015-05-07-primera-division-voronoi.md
+++ posts/2015-05-07-primera-division-voronoi.md
@@ -1,17 +1,42 @@
 ---
 title: Primera Division Voronoi
 author: Ezequiel A. Alvarez
-tags: introduction
+tags: cartogramas, futbol, d3
 ---
-<style>
-path {
-  stroke-linejoin: round;
-}
-</style>
-<div id="container"></div>
 
-This map ...
+Este es un mapa de los estadios de los equipos de Primera División
+del fútbol argentino a la fecha.
+Tomando cada estadio como un punto en el plano, trazamos el diagrama voronoi que forman.
+
+<div id="container-full"></div>
+
+Detalle de Buenos Aires.
+
+<div id="container-small"></div>
 
 <script src="/js/async.js"></script>
 <script src="/js/topojson.min.js"></script>
 <script src="/js/provincias-voronoi.js"></script>
+<style>
+path {
+    stroke-linejoin: round;
+    pointer-events: all;
+    fill: none;
+}
+.active path {
+    fill: rgba(36, 132, 193, 0.22);
+}
+.label {
+    font-family: verdana;
+    font-size: 16px;
+    fill: black;
+    cursor: default;
+}
+.label-small {
+    font-size: 11px;
+    fill: #222;
+}
+path.dimmed {
+    opacity: 0.1;
+}
+</style>
